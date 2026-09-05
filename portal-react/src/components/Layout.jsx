8c214e1b4e17fbf2aa17b2d70feb150d66eb726d
hoenotfound <91048208+hoenotfound@users.noreply.github.com>
2026-09-05 16:37:48 +0800

    Remove dashboard DOM language adapter

--- portal-react/src/components/Layout.jsx
+++ portal-react/src/components/Layout.jsx
@@ -1,11 +1,9 @@
 import Sidebar from "./Sidebar";
 import LiveAcquisitionBar from "./LiveAcquisitionBar";
-import IndustryLanguageAdapter from "./IndustryLanguageAdapter";
 
 export default function Layout({ children }) {
   return (
     <div className="relative flex h-[100dvh] overflow-hidden md:h-screen">
-      <IndustryLanguageAdapter />
       <Sidebar />
       <main className="flex h-[calc(100dvh-4.5rem)] min-w-0 flex-1 flex-col overflow-hidden md:h-screen">
         <LiveAcquisitionBar />
